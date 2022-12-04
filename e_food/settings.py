ce4701f69eb382626c48182688c53a5a0d830c42
SriMadhanSA <madhanreddy.1995@gmail.com>
2022-12-04 19:46:55 +0300

    Update email notification via DjangoSES

--- e_food/settings.py
+++ e_food/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -142,3 +141,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django_ses.SESBackend'
